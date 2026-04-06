4419644a49c1371bfbb03615b994caf91cb6a2cd
Viktor <perfiljevvicktor@yandex.ru>
2026-04-06 14:04:03 +0700

    Syghtrswe

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,6 @@
 import {AdminPage} from "@/src/features/admin/components/AdminPage";
-
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 
 export default function Admin(){
     return <AdminPage/>
