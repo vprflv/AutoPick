7518b7078d8cb7fde9ccaf72f93b18f963524a01
Viktor <perfiljevvicktor@yandex.ru>
2026-04-10 21:41:49 +0700

    Work Variant + callback work + image work + docker_work_v + correct_code + correct_style + tests

--- src/features/catalog/components/CarCard/CarCardGallery/CarCardGallery.tsx
+++ src/features/catalog/components/CarCard/CarCardGallery/CarCardGallery.tsx
@@ -30,12 +30,12 @@ export function CarCardGallery({handleTouchStart,
                                    images}:CarCardGalleryProps) {
     return (
 
-        <div>
-            <div className="relative"
-                 onTouchStart={handleTouchStart}
-                 onTouchMove={handleTouchMove}
-                 onTouchEnd={handleTouchEnd}
-
+        <div className="relative">
+            <div
+                className="relative overflow-hidden"
+                onTouchStart={handleTouchStart}
+                onTouchMove={handleTouchMove}
+                onTouchEnd={handleTouchEnd}
             >
                 <Image
                     src={mainImage}
@@ -43,48 +43,49 @@ export function CarCardGallery({handleTouchStart,
                     width={800}
                     height={520}
                     className="w-full aspect-[16/10] object-cover"
+                    priority={false}
                 />
 
+                {/* Стрелки  */}
                 {hasMultipleImages && (
-                    /* Стрелки для листания (показываем только если фото > 1) */
                     <>
                         {/* Левая стрелка */}
                         <button
                             onClick={goToPrevious}
                             className="absolute left-6 top-1/2 -translate-y-1/2
-                       w-11 h-11
-                       border border-white/10 hover:border-white/10
-                       bg-black/10 hover:bg-black/20
-                       rounded-2xl
-                       transition-all active:scale-95 z-20
-                       flex items-center justify-center"
+                                       w-11 h-11
+                                       border border-white/10 hover:border-white/10
+                                       bg-black/10 hover:bg-black/20
+                                       rounded-2xl
+                                       transition-all active:scale-95 z-20
+                                       flex items-center justify-center"
                         >
-                            <span className=" mb-2 text-4xl font-light text-blue-500 drop-shadow-md">‹</span>
+                            <span className="mb-2 text-4xl font-light text-blue-500 drop-shadow-md">‹</span>
                         </button>
 
                         {/* Правая стрелка */}
                         <button
                             onClick={goToNext}
                             className="absolute right-6 top-1/2 -translate-y-1/2
-                       w-11 h-11
-                       border border-white/10 hover:border-white/10
-                       bg-black/10 hover:bg-black/20
-                       rounded-2xl
-                       transition-all active:scale-95 z-20
-                       flex items-center justify-center"
+                                       w-11 h-11
+                                       border border-white/10 hover:border-white/10
+                                       bg-black/10 hover:bg-black/20
+                                       rounded-2xl
+                                       transition-all active:scale-95 z-20
+                                       flex items-center justify-center"
                         >
-                            <span className=" mb-2 text-4xl font-light text-blue-500 drop-shadow-md">›</span>
+                            <span className="mb-2 text-4xl font-light text-blue-500 drop-shadow-md">›</span>
                         </button>
 
-                        {/* Индикатор текущего фото */}
+                        {/* Индикатор */}
                         <div
-                            className="absolute bottom-3 left-1/2 -translate-x-1/2 bg-black/60 text-white text-xs px-3 py-1 rounded-full">
+                            className="absolute bottom-3 left-1/2 -translate-x-1/2
+                                       bg-black/60 text-white text-xs px-3 py-1 rounded-full backdrop-blur-md"
+                        >
                             {currentImageIndex + 1} / {images.length}
                         </div>
                     </>
-
                 )}
-
             </div>
         </div>
     )
